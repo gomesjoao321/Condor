f32359e8e2b4cf9329a292bb0bcbf5dc84b1f399
gomesjoao321 <101751302+gomesjoao321@users.noreply.github.com>
2022-07-29 10:50:29 -0300

    Add files via upload

---
+++ Models/UpdateQuantidadeResponse.cs
@@ -0,0 +1,20 @@
+﻿using CondorBicicletas.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CondorBicicletas.Models
+{
+    public class UpdateQuantidadeResponse
+    {
+        public UpdateQuantidadeResponse(ItemPedido itemPedido, CarrinhoViewModel carrinhoViewModel)
+        {
+            ItemPedido = itemPedido;
+            CarrinhoViewModel = carrinhoViewModel;
+        }
+
+        public ItemPedido ItemPedido { get; }
+        public CarrinhoViewModel CarrinhoViewModel { get; }
+    }
+}
